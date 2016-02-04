b87a8d4812a6dcd225ad462a8554ca8c7a55045b
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-04 15:24:16 -0500

    Add 'convert' method for CurrencyConverter class that takes in a new currency code and returns a Currency object

--- code.rb
+++ code.rb
@@ -1,15 +1,18 @@
 require './currency.rb'
 require './currency_converter.rb'
+require 'byebug'
 
 usd_one = Currency.new(10, :USD)
 usd_two = Currency.new(15, :USD)
 cad_one = Currency.new(10, :CAD)
 
-puts "enter a currency"
-cad_two = Currency.new(gets.chomp)
+# puts "enter a currency"
+cad_two = Currency.new("20", "USD")
 
 rates = CurrencyConverter.new
-puts rates.find_rate(cad_two.code.to_sym)
+# puts rates.find_rate(cad_two.code.to_sym)
+new_currency = rates.convert(cad_two, :USD)
+puts "#{new_currency.amount} #{new_currency.code}"
 
 # puts cad_two.amount
 # puts cad_two.code
--- currency_converter.rb
+++ currency_converter.rb
@@ -6,4 +6,10 @@ class CurrencyConverter
   def find_rate(code)
     @rates[code]
   end
+
+  def convert(currency, new_code)
+    coverted_currency = Currency.new((currency.amount.to_f * (find_rate(new_code) / find_rate(currency.code.to_sym))), new_code.to_s)
+    # currency.multiply(find_rate(new_code) / find_rate(currency.code.to_sym)
+    # currency.code
+  end
 end
