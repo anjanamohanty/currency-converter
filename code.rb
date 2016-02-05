2912342d3cd1bb210e22a30e157eaf0f5692f603
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-05 18:35:00 -0500

    Update CurrencyTrader class to take in an array of CurrencyConverter objects, rather than two explicitly

--- code.rb
+++ code.rb
@@ -2,28 +2,12 @@ require './currency.rb'
 require './currency_converter.rb'
 require './currency_trader.rb'
 
-usd_one = Currency.new(10, :USD)
-usd_two = Currency.new(15, :USD)
-cad_one = Currency.new(10, :CAD)
-
-usd_three = usd_one.add(usd_two)
-puts "#{usd_three.amount} #{usd_three.code}"
-
-usd_four = usd_three.subtract(usd_two)
-puts "#{usd_four.amount} #{usd_four.code}"
-
-usd = usd_four.multiply(4)
-puts "#{usd.amount} #{usd.code}"
+hundred_usd = Currency.new(100, :EUR)
 
 old_value = CurrencyConverter.new({USD: 1, EUR: 0.767, GBP: 0.642, AUD: 1.353, CAD: 1.355, JPY: 102.162})
-present_value = CurrencyConverter.new({USD: 1, EUR: 0.898, GBP: 0.556, AUD: 1.433, CAD: 1.322, JPY: 119.863})
-# #
-# # monday = CurrencyConverter.new(rates)
-# # tuesday = CurrencyConverter.new(rates_tomorrow)
-# # usd_to_cad = monday.convert(usd, :EUR)
-#
-# puts usd_to_cad.amount
-# puts usd_to_cad.code
+present_value = CurrencyConverter.new({USD: 1, EUR: 0.898, GBP: 0.656, AUD: 1.433, CAD: 1.122, JPY: 119.863})
+future_value = CurrencyConverter.new({USD: 1, EUR: 0.898, GBP: 0.656, AUD: 1.433, CAD: 1.322, JPY: 119.863})
+another_future_value = CurrencyConverter.new({USD: 1, EUR: 0.767, GBP: 0.642, AUD: 1.353, CAD: 1.355, JPY: 102.162})
 
-trader = CurrencyTrader.new(old_value, present_value, usd_one)
-puts "#{trader.trade}!"
+trader = CurrencyTrader.new([old_value, present_value, future_value, another_future_value], hundred_usd)
+trader.trade
--- currency_trader.rb
+++ currency_trader.rb
@@ -1,15 +1,31 @@
 class CurrencyTrader
-  def initialize(converter_one, converter_two, currency)
-    @converter_one = converter_one
-    @converter_two = converter_two
+  def initialize(converter_array, currency)
+    @converter_array = converter_array
     @currency = currency
   end
 
   def trade
-    good_rates = {}
-    @converter_one.rates.each{ |key, value|
-      (@converter_one.rates[key] > @converter_two.rates[key]) ? (good_rates[key] = ((@converter_one.rates[key] / @converter_one.rates[@currency.code.to_sym]) * (@converter_two.rates[@currency.code.to_sym] / @converter_two.rates[key]))) : next
+    trades = []
+    trades[0] = @currency.code
+    @converter_array.length.times do |i|
+      break if (i+1) == @converter_array.length
+      trades[i+1] = get_best_trade(i, trades[i])
+    end
+    puts trades
+
+    # trades.length.times do |i|
+    #   break if (i+1) == trades.length
+    #   puts "You should invest in #{trades}"
+  end
+
+  def get_best_trade(i, current_currency)
+    rates = {}
+    @converter_array[i].rates.each{ |key, value|
+      rates[key] = ((@converter_array[i].rates[key] / @converter_array[i].rates[current_currency.to_sym]) * (@converter_array[i+1].rates[current_currency.to_sym] / @converter_array[i+1].rates[key]))
     }
-    return good_rates.key(good_rates.values.max)
+    best_trade = rates.key(rates.values.max)
+    best_trade_rate = rates[best_trade]
+    puts "#{best_trade} #{best_trade_rate}"
+    return best_trade
   end
 end
